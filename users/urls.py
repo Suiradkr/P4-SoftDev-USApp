c18e949e1ffbce159a7aa3d47664e59a4d7ca09a
Darius Roberts <dariusroberts@MacBookPro.lan>
2025-11-03 17:16:33 -0500

    added sign up page

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import ProfileView
+from .views import ProfileView, SignupView
 
 app_name = 'users'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', next_page='/users/profile/'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/users/login'), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('signup/', SignupView.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.views import View
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
-    """Simple profile page that requires authentication."""
-    template_name = 'users/profile.html'
 
-    # Optionally customize the login redirect URL (defaults to settings.LOGIN_URL)
+    template_name = 'users/profile.html'
     login_url = '/users/login/'
 
     def get_context_data(self, **kwargs):
@@ -14,3 +16,45 @@ class ProfileView(LoginRequiredMixin, TemplateView):
         # expose the user in the template context (TemplateView already provides request via context processors)
         context['user'] = self.request.user
         return context
+
+
+class SignupView(View):
+
+    def get(self, request):
+        # redirect authenticated users to profile
+        if request.user.is_authenticated:
+            return redirect('users:profile')
+        return render(request, 'users/signup.html', {'form_data': {}})
+
+    def post(self, request):
+        if request.user.is_authenticated:
+            return redirect('users:profile')
+        
+        username = request.POST.get('username', '').strip()
+        password1 = request.POST.get('password1', '')
+        password2 = request.POST.get('password2', '')
+        errors = []
+
+        User = get_user_model()
+        if not username:
+            errors.append('Username is required.')
+        elif User.objects.filter(username=username).exists():
+            errors.append('This username is already taken.')
+
+        if not password1 or not password2:
+            errors.append('Both password fields are required.')
+        elif password1 != password2:
+            errors.append('Passwords do not match.')
+
+        if errors:
+            return render(request, 'users/signup.html', {
+                'errors': errors,
+                'form_data': {'username': username},
+            })
+
+        # create user
+        user = User.objects.create_user(username=username, password=password1)
+        return render(request, 'users/signup.html', {
+            'created': True,
+            'username': user.username,
+        })
